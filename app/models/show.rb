b18747e8dbd9316fada0e593f4edcc4a3c40c7b2
Christopher Dent <christopherdent01@gmail.com>
2019-11-10 22:35:08 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,17 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+  #  actor.shows << self.actors
+   binding.pry
+  #  Show.create(name: ?)
+
+  end
+
+
+
 end
+
+#Person.where("administrator = 1")
